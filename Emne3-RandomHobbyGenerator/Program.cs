052b72d6fecbb936ea1b85ea2128ac31a2c874b4
Biner Habib <Binerhabib@gmail.com>
2023-11-15 10:34:13 +0100

    Add project files.

---
+++ Emne3-RandomHobbyGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Emne3_RandomHobbyGenerator
+{
+    internal class Program
+    {
+        string 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
